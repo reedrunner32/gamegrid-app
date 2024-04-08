53a00b7811a32594951a6f2de33da93272da4c2a
BuildTools <richardzhang32@gmail.com>
2024-04-08 01:05:12 -0400

    Added exception case for failed http fetch

--- lib/utils/getAPI.dart
+++ lib/utils/getAPI.dart
@@ -16,7 +16,12 @@ class CardsData {
           body: utf8.encode(outgoing),
           encoding: Encoding.getByName("utf-8")
       );
-      ret = response.body;
+      if (response.statusCode == 200) {
+        ret = response.body;
+      }
+      else {
+        throw Exception('Failed to load data');
+      }
     }
     catch (e)
     {
