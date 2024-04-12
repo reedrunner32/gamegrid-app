9cc1838c4df694b8333b901670c2296896e35c21
BuildTools <richardzhang32@gmail.com>
2024-04-12 17:35:55 -0400

    Minor formatting changes

--- lib/screens/GameScreen.dart
+++ lib/screens/GameScreen.dart
@@ -15,17 +15,82 @@ class GameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final game = ModalRoute.of(context)!.settings.arguments as GameInfo;
-
+    Color text_color = const Color.fromRGBO(155, 168, 183, 1);
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text(game.name),
-      ),
-      body: Column(
-        children: [
-          Image.network(game.imageURL),
-          Text(game.description),
+        backgroundColor: Colors.transparent,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.black38,
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child: Icon(Icons.arrow_left, size: 35, color: Colors.white,),
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Container(
+              margin: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Colors.black38,
+                  borderRadius: BorderRadius.circular(20)
+              ),
+              child: Icon(Icons.more_horiz_outlined, size: 35, color: Colors.white,),
+            ),
+          ),
         ],
       ),
+      backgroundColor: Color.fromRGBO(25, 28, 33, 1),
+      body: SingleChildScrollView(
+        child:
+        Stack(
+          children: [
+            Container(
+              child:
+              Column(
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(game.name, style: TextStyle(color: Colors.white, fontWeight: FontWeight.w800, fontSize: 24), )
+                ),
+
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(width: 0.5, color: text_color)
+                  ),
+                  child:
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child:
+                    Image.network(game.imageURL, scale: 1.7,),
+                  )
+                ),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child:
+                  Text(game.description, style: TextStyle(color: text_color, fontSize: 14),),
+                ),
+
+              ]
+            )
+            )
+          ],
+        ),
+      )
     );
   }
 }
