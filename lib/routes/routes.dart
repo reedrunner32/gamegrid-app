0b9c7e736d1a1a1546c59f2f135cab29854fbd1d
BuildTools <richardzhang32@gmail.com>
2024-04-17 19:48:14 -0400

    Removed unused screens (login, register, cards)

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:gamegrid/screens/LoginScreen.dart';
-import 'package:gamegrid/screens/CardsScreen.dart';
-import 'package:gamegrid/screens/RegisterScreen.dart';
 import 'package:gamegrid/screens/HomeScreen.dart';
 import 'package:gamegrid/screens/ContentScreen.dart';
 
 class Routes {
-  static const String LOGINSCREEN = '/login';
-  static const String CARDSSCREEN = '/cards';
-  static const String REGISTERSCREEN = '/register';
   static const String HOMESCREEN = '/home';
   static const String CONTENTSCREEN = '/content';
 // routes of pages in the app
   static Map<String, Widget Function(BuildContext)> get getroutes => {
     '/': (context) => HomeScreen(),
-    LOGINSCREEN: (context) => LoginScreen(),
-    CARDSSCREEN: (context) => CardsScreen(),
-    REGISTERSCREEN: (context) => RegisterScreen(),
     HOMESCREEN: (context) => HomeScreen(),
     CONTENTSCREEN: (context) => ContentScreen(),
   };
